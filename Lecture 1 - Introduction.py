3ce8724e3ec07cf1df2a60e3b278635eaffa5335
martynasp3 <martynasp1313@gmail.com>
2025-02-22 23:57:48 +0000

    Lecture 1 completed

---
+++ Lecture 1 - Introduction.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# <QUESTION 1>
+# Assume 3 variables are already defined for you: a, b, and c.
+# Create a variable called total that adds a and b
+# then multiplies the result by c.
+# Include a last line in your code to print the value: print(total)
+
+# I defined the variables for functionality purposes.
+a = 1
+b = 2
+c = 4
+total = (a+b)*c
+print(total)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
